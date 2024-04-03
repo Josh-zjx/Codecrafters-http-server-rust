615ac695afbd80d6ab6e35150fc511297c04943a
Jiaxu Zhu <josh.zhujiaxu@gmail.com>
2024-04-03 11:00:24 +0800

    Add common status code header to Const

--- src/main.rs
+++ src/main.rs
@@ -14,23 +14,21 @@ pub struct Opt {
     pub _directory: PathBuf,
 }
 
+const RES_404: &[u8] = b"HTTP/1.1 404 Not Found\r\n\r\n";
+const RES_201: &[u8] = b"HTTP/1.1 201 Created\r\n\r\n";
+
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
     let opt = Opt::from_args();
     let current_directory = Box::leak(Box::new(opt._directory));
 
-    // Uncomment this block to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    //
+
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
                 std::thread::spawn(|| {
                     handle_client(_stream, current_directory);
                 });
-                //handle_client(_stream);
                 println!("accepted new connection");
             }
             Err(e) => {
@@ -54,11 +52,9 @@ fn handle_client(mut stream: TcpStream, current_directory: &Path) {
                 let file_path = current_directory.join(filename);
                 if let Ok(mut file) = std::fs::File::create(file_path) {
                     file.write_all(lines.last().unwrap().as_bytes()).unwrap();
-                    stream.write_all(b"HTTP/1.1 201 Created\r\n\r\n").unwrap();
+                    stream.write_all(RES_201).unwrap();
                 } else {
-                    stream
-                        .write_all(generate_response(404, "", "").as_bytes())
-                        .unwrap();
+                    stream.write_all(RES_404).unwrap();
                 }
             }
         }
@@ -67,49 +63,44 @@ fn handle_client(mut stream: TcpStream, current_directory: &Path) {
                 stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
             } else if let Some(content) = path.strip_prefix("/echo/") {
                 stream
-                    .write_all(generate_response(200, content, "text/plain").as_bytes())
+                    .write_all(generate_response(content, "text/plain").as_bytes())
                     .unwrap();
             } else if *path == "/user-agent" {
-                let (status, content) = if let Some(line) = lines.get(2) {
+                if let Some(line) = lines.get(2) {
                     if let Some(content) = line.strip_prefix("User-Agent: ") {
-                        (200, content)
+                        stream
+                            .write_all(generate_response(content, "text/plain").as_bytes())
+                            .unwrap();
                     } else {
-                        (404, "")
+                        stream.write_all(RES_404).unwrap();
                     }
                 } else {
-                    (404, "")
+                    stream.write_all(RES_404).unwrap();
                 };
-                stream
-                    .write_all(generate_response(status, content, "text/plain").as_bytes())
-                    .unwrap();
             } else if let Some(filename) = path.strip_prefix("/files/") {
                 let file_path = current_directory.join(filename);
                 if let Ok(content) = read_to_string(file_path) {
                     stream
                         .write_all(
-                            generate_response(200, &content, "application/octet-stream").as_bytes(),
+                            generate_response(&content, "application/octet-stream").as_bytes(),
                         )
                         .unwrap();
                 } else {
-                    stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n").unwrap();
+                    stream.write_all(RES_404).unwrap();
                 }
             } else {
-                stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n").unwrap();
+                stream.write_all(RES_404).unwrap();
             }
         }
         _default => {}
     }
 }
 
-fn generate_response(status: usize, text: &str, content_type: &str) -> String {
-    if status == 404 {
-        "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
-    } else {
-        format!(
-            "HTTP/1.1 200 OK\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n{}\r\n",
-            content_type,
-            text.len(),
-            text
-        )
-    }
+fn generate_response(text: &str, content_type: &str) -> String {
+    format!(
+        "HTTP/1.1 200 OK\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n{}\r\n",
+        content_type,
+        text.len(),
+        text
+    )
 }

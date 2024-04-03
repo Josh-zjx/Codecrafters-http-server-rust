90a1bb1ebdba5e658ae62d20aaca9fc9ded6b2a8
Jiaxu Zhu <josh.zhujiaxu@gmail.com>
2024-04-03 09:31:24 +0800

    implement naive file GET

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
+use core::str;
+use std::fs::read_to_string;
+use std::path::Path;
 // Uncomment this block to pass the first stage
 use std::io::{Read, Write};
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::path::PathBuf;
+use structopt::StructOpt;
 
-//const NOT_FOUND: String = String::from("HTTP/1.1 404 Not Found\r\n");
+#[derive(Debug, StructOpt)]
+pub struct Opt {
+    #[structopt(long, parse(from_os_str), default_value = "")]
+    pub _directory: PathBuf,
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
+    let opt = Opt::from_args();
+    let current_directory = Box::leak(Box::new(opt._directory));
 
     // Uncomment this block to pass the first stage
     //
@@ -17,7 +28,7 @@ fn main() {
         match stream {
             Ok(_stream) => {
                 std::thread::spawn(|| {
-                    handle_client(_stream);
+                    handle_client(_stream, current_directory);
                 });
                 //handle_client(_stream);
                 println!("accepted new connection");
@@ -29,7 +40,7 @@ fn main() {
     }
 }
 
-fn handle_client(mut stream: TcpStream) {
+fn handle_client(mut stream: TcpStream, current_directory: &Path) {
     let mut buf = [0; 256];
     let length = stream.read(&mut buf).unwrap();
     let input = String::from_utf8(buf[..length].to_vec()).unwrap();
@@ -40,7 +51,7 @@ fn handle_client(mut stream: TcpStream) {
         stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
     } else if let Some(content) = path.strip_prefix("/echo/") {
         stream
-            .write_all(generate_response(200, content).as_bytes())
+            .write_all(generate_response(200, content, "text/plain").as_bytes())
             .unwrap();
     } else if path == &"/user-agent" {
         let (status, content) = if let Some(line) = lines.get(2) {
@@ -53,19 +64,29 @@ fn handle_client(mut stream: TcpStream) {
             (404, "")
         };
         stream
-            .write_all(generate_response(status, content).as_bytes())
+            .write_all(generate_response(status, content, "text/plain").as_bytes())
             .unwrap();
+    } else if let Some(filename) = path.strip_prefix("/files/") {
+        let file_path = current_directory.join(filename);
+        if let Ok(content) = read_to_string(file_path) {
+            stream
+                .write_all(generate_response(200, &content, "application/octet-stream").as_bytes())
+                .unwrap();
+        } else {
+            stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n").unwrap();
+        }
     } else {
         stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n").unwrap();
     }
 }
 
-fn generate_response(status: usize, text: &str) -> String {
+fn generate_response(status: usize, text: &str, content_type: &str) -> String {
     if status == 404 {
         "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
     } else {
         format!(
-            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n",
+            "HTTP/1.1 200 OK\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n{}\r\n",
+            content_type,
             text.len(),
             text
         )
